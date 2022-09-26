a9cf0b22afd91a79ca857f81de5de615581f62e6
Zachary Casares <rrumble250@gmail.com>
2022-09-26 14:50:36 -0700

    add bcrypt to app & password digest to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-before_save{ self.email = email.downcase }    
-has_many :articles
+    before_save{ self.email = email.downcase }    
+    has_many :articles
+
 
     validates :username, presence: true,
                             uniqueness: {case_sensitive: false},
@@ -12,6 +13,6 @@ has_many :articles
                         uniqueness: {case_sensitive: false},
                         length: {maximum:105},
                         format: {with: VALID_EMAIL_REGEX}
-
+    has_secure_password
 
 end
