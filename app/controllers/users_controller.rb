2a55f78d6919560cc50482eb690a0d1cf6867d8f
Zachary Casares <rrumble250@gmail.com>
2022-09-27 11:42:30 -0700

    add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
 
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+
+    end
+
     def new
         @user = User.new
     end
