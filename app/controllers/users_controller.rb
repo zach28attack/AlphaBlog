24c4d8c388064506819f2e95762ae5cf4d9aa333
Zachary Casares <rrumble250@gmail.com>
2022-09-28 10:40:16 -0700

    cleanup layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,8 +45,11 @@ class UsersController < ApplicationController
 
     private 
     def user_params
-        params.require(:user).permit(:username, :email, :password)
+        params.require(:user).permit(:username, :email, :password)  
 
     end
 
 end
+
+
+ 
