a31db217b467e2f0396dcdfc7aa1a82dfbb6b53e
Zachary Casares <rrumble250@gmail.com>
2022-09-28 16:39:31 -0700

    refactored users controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
         image_tag(gravatar_url,atl: user.username, class: "rounded shadow mx-auto d-block")
 
     end
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if  session[:user_id]
+            
+    end
+
+    def logged_in?
+        !!current_user 
+    end
+
 end
