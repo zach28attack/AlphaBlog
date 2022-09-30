24c577c374b90556038c16c8c6f8885eb34485f1
Zachary Casares <rrumble250@gmail.com>
2022-09-30 10:42:48 -0700

    added categories controller, views, and tests

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,32 @@
+class CategoriesController < ApplicationController
+    def new
+        @category = Category.new
+        
+    end
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "Category was created successfully"
+            redirect_to @category 
+
+        else 
+            render 'new', status: :unprocessable_entity
+        end
+    end
+
+    def index
+        
+    end
+
+    def show
+        
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
+        
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy' #delete -> get
+  resources :categories, except: [:destroy]
   
   
 end
