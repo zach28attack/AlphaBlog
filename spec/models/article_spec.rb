b6dd50ccbcdb898403478d61d560aca795454388
Zachary Casares <rrumble250@gmail.com>
2023-03-17 12:36:52 -0400

    add validation test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {FactoryBot.create(:user)}
+  let(:article) {FactoryBot.create(:article, user: user)}
+  context "with valid attributes" do
+    it "should create article" do
+      expect(article).to be_valid
+    end
+  end
 end
