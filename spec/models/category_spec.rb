d368f2d9978bafbdaa0b9c326af4d2f957094d26
Zachary Casares <rrumble250@gmail.com>
2023-03-18 13:53:06 -0400

    add category model validation tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+let(:category) {FactoryBot.create(:category)}
+  context "with valid name" do
+    it "should be valid" do 
+      expect(category).to be_valid
+    end
+  end
+
+  context "with invalid name" do
+    it "should be invalid" do 
+      category.name = ""
+      expect(category).to_not be_valid
+    end
+  end
 end
