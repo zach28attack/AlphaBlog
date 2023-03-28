c4b85f2bfd510a66e0688c8ced9e05c1bbaf87fc
Zachary Casares <rrumble250@gmail.com>
2023-03-28 14:06:37 -0400

    increase max and min characters allowed

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,5 @@ class Article < ApplicationRecord
     has_many :article_categories
     has_many :categories, through: :article_categories
     validates :title, presence: true, length: {minimum: 5, maximum:100}
-    validates :body, presence: true, length: {minimum: 10, maximum: 300}
+    validates :body, presence: true, length: {minimum: 25, maximum: 600}
 end
