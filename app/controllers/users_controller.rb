f4ea41328e22873825d0c91bb98c73c2a4ea0dec
Zachary Casares <rrumble250@gmail.com>
2023-03-29 20:36:05 -0400

    increase max number of users per page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,11 @@ class UsersController < ApplicationController
 
     def show
         @articles = @user.articles.paginate(:page => params[:page], :per_page => 5)
+        
     end
 
     def index
-        @users = User.paginate(:page => params[:page], :per_page => 5)
+        @users = User.paginate(:page => params[:page], :per_page => 8)
     end
 
     def new
