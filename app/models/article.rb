bcca86dea80818a9ab5c6558cfd0fd7d2f1f02cf
Zachary Casares <rrumble250@gmail.com>
2023-03-29 21:42:33 -0400

    reduce maximum title length

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
     belongs_to :user
     has_many :article_categories
     has_many :categories, through: :article_categories
-    validates :title, presence: true, length: {minimum: 5, maximum:100}
+    validates :title, presence: true, length: {minimum: 5, maximum:12}
     validates :body, presence: true, length: {minimum: 25, maximum: 600}
 end
