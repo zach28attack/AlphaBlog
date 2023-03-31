7d6dc44e169a0fcc3f7382df918e9cfb4a443661
Zachary Casares <rrumble250@gmail.com>
2023-03-31 16:04:42 -0400

    tweak validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,15 @@ class User < ApplicationRecord
     has_many :articles, dependent: :destroy
     validates :username, presence: true,
                             uniqueness: {case_sensitive: false},
-                            length: {minimum: 3, maximum:25}
+                            length: {minimum: 3, maximum:15}
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
     validates :email, presence: true,
                         uniqueness: {case_sensitive: false},
-                        length: {minimim: 11, maximum:105}, #minimum length being "@" ".com" + 3 minimum for email and domain 
                         format: {with: VALID_EMAIL_REGEX}
+
+    validates :password, presence: true, length: {minimum: 6, maximum: 20}
     has_secure_password
     self.per_page = 5
 end
