827f8b6e85fa9b2d13f6c13bdc9ef4381904422e
Zachary Casares <rrumble250@gmail.com>
2023-03-31 17:19:07 -0400

    whitelist images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
 
     private 
     def user_params
-        params.require(:user).permit(:username, :email, :password)  
+        params.require(:user).permit(:username, :email, :password, :image)  
     end
 
     def set_user
